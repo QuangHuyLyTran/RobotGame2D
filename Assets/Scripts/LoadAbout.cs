9146e88b476dec7f1ef3f540f7d692fb63b2a770
Quang <quanghuylytran@gmail.com>
2022-05-04 15:59:43 +0700

    UnityGame2D

---
+++ Assets/Scripts/LoadAbout.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAbout : MonoBehaviour
+{
+    public void Load()
+    {
+        Application.LoadLevel("Level1");
+    }
+}
